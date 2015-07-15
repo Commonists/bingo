9c840167528d9547337cb9418302bf0082fae7bb
Jean-Fred <JeanFred@github>
2015-07-15 16:49:18 -0500

    Add method select_words
    With two unit tests.

--- bingo/bingo.py
+++ bingo/bingo.py
@@ -1,3 +1,4 @@
+import random
 
 DEFAULT_SIZE = 20
 
@@ -9,3 +10,6 @@ class BingoGenerator(object):
     def __init__(self, size=DEFAULT_SIZE):
         self.words = None
         self.size = size
+
+    def select_words(self):
+        return random.sample(self.words, self.size)
--- tests/test_bingo.py
+++ tests/test_bingo.py
@@ -15,3 +15,19 @@ class TestBingoGenerator(unittest.TestCase):
     def test_bingo_generator_has_given_size(self):
         bingo_generator = bingo.BingoGenerator(5)
         self.assertEquals(bingo_generator.size, 5)
+
+    def test_select_words_should_have_the_right_size(self):
+        test_size = 5
+        bingo_generator = bingo.BingoGenerator(size=test_size)
+        seed_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+        bingo_generator.words = seed_list
+        selection = bingo_generator.select_words()
+        self.assertEquals(len(selection), test_size)
+
+    def test_select_words_should_return_words_from_the_seed_list(self):
+        test_size = 5
+        bingo_generator = bingo.BingoGenerator(size=test_size)
+        seed_list = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'])
+        bingo_generator.words = seed_list
+        selection = set(bingo_generator.select_words())
+        self.assertTrue(seed_list.difference(selection))
