4eb7bc5c8fdd701ad78b1fc1eeab4c941e0c4fb7
Jean-Fred <JeanFred@github>
2015-07-15 16:05:50 -0500

    Add infrastucture
    With tox and flake8

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = ['bingo']
+requires = ['argparse']
+scripts = []
+
+setup(
+    name='Bingo',
+    version='0.1',
+    author='Jean-Frédéric',
+    author_email='jeanfrederic.wiki@gmail.com',
+    url='https://github.com.org/JeanFred/bingo',
+    description='Tool to generate bingo grids.',
+    license='MIT',
+    packages=packages,
+    install_requires=requires,
+    scripts=scripts,
+)
