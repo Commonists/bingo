cc18fc655bf24fdcecf4ad00b049673f6dbd2db6
Jean-Fred <JeanFred@github>
2015-07-15 16:39:27 -0500

    Add unit test test_bingo_generator_has_given_size

--- tests/test_bingo.py
+++ tests/test_bingo.py
@@ -4,10 +4,14 @@ import unittest
 from bingo import bingo
 
 
-class Test(unittest.TestCase):
+class TestBingoGenerator(unittest.TestCase):
 
     """Test methods from bingo."""
 
     def test_bingo_generator_has_default_size(self):
         bingo_generator = bingo.BingoGenerator()
         self.assertEquals(bingo_generator.size, 20)
+
+    def test_bingo_generator_has_given_size(self):
+        bingo_generator = bingo.BingoGenerator(5)
+        self.assertEquals(bingo_generator.size, 5)
